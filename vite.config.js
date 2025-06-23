99debf99375ef50067a5c463b26191239aa2cf8f
MaleficiumT <timonina.maria.356@mail.ru>
2025-06-23 18:00:43 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'src/pages/about/about.html'),
+        services: resolve(__dirname, 'src/pages/services/services.html'),
+				merits: resolve(__dirname, 'src/pages/merits/merits.html'),
+				studios: resolve(__dirname, 'src/pages/studios/studios.html'),
+        login: resolve(__dirname, 'src/pages/login/login.html')
+      }
+    }
+  }
+});
